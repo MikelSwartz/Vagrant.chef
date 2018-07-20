e81b8111f9d70c3268aa820f73770965da53dbde
Mike <mikelswartz@gmail.com>
2018-07-20 06:50:49 -0400

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,13 @@
+Vagrant.configure("2") do |config|
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 2048
+  end
+  config.vm.define "chef" do |chef|
+          chef.vm.box = "ubuntu/trusty64"
+          chef.vm.hostname = "chef"
+          chef.vm.network "forwarded_port", guest: 80, host: 80
+          chef.vm.network "forwarded_port", guest: 8080, host: 8080
+          chef.vm.provision "shell",
+            path: "chef.provision.sh"
+  end
+end
---
+++ chef.provision.sh
@@ -0,0 +1,24 @@
+curl -L https://www.opscode.com/chef/install.sh | bash
+apt-get install git tree -y 
+wget http://github.com/opscode/chef-repo/tarball/master
+mkdir /root/chef-repo
+tar -zxf master -C /root/
+mv /root/chef-boneyard*/* /root/chef-repo/
+rm master chef-boneyard* -rf
+cd /root/chef-repo/
+mkdir .chef
+echo "cookbook_path [ '/root/chef-repo/cookbooks' ]" > .chef/knife.rb
+
+cd /root/chef-repo/
+
+echo <<END > solo.rb
+file_cache_path \"/root/chef-solo\"
+cookbook_path \"/root/chef-repo/cookbooks\"
+END
+
+echo <<END > web.json
+{
+	  "run_list": [ "recipe[apt]", "recipe[httpd]" ]
+}
+END
+
